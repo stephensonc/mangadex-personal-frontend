cdb3ca834fa4df6c4a8f7c851d77017bd6483c8f
Chris Stephenson <stephensonc@allegheny.edu>
2021-05-22 02:14:20 -0400

    Add credential parsing to UserProfile

--- src/UserProfile.py
+++ src/UserProfile.py
@@ -1,8 +1,14 @@
 import yaml
+import requests
+
 
 class UserProfile:
     
     def __init__(self) -> None:
-        with open("../credentials.yml") as configfile:
+
+        self.mangadex_url_base = "https://api.mangadex.org"
+
+        with open("../config.yml") as configfile:
             self.config = yaml.safe_load(configfile)
-        self.username = 
+        self.username = self.config["user_credentials"]["username"]
+        self.password = self.config["user_credentials"]["password"]
