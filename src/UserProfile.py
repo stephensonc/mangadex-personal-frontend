9fdbc41efb6636b7655d0f0ae0bb69781d4b2736
Chris Stephenson <stephensonc@allegheny.edu>
2021-05-22 02:35:34 -0400

    Add user authentication functions

--- src/UserProfile.py
+++ src/UserProfile.py
@@ -12,3 +12,19 @@ class UserProfile:
             self.config = yaml.safe_load(configfile)
         self.username = self.config["user_credentials"]["username"]
         self.password = self.config["user_credentials"]["password"]
+        self.api_is_online = self.check_api_status()
+        if self.api_is_online:
+            print(f"Authentication status code: {self.authenticate_user()}")
+        
+    def check_api_status(self) -> bool:
+        # Verify that the api auth system is online
+        return requests.get(self.mangadex_url_base + "/auth/check").status_code == 200
+
+    def authenticate_user(self) -> int:
+        print(f"Authenticating user: {self.username}")
+        request_data = {
+            "username": self.username,
+            "password": self.password
+        }
+        self.auth_info = requests.post(self.mangadex_url_base + "/auth/login", json=request_data)
+        return self.auth_info.status_code
