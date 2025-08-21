d2d0cc5b3a1542cdab66a064381d9b369ba3424a
Chris Stephenson <cgstephenson675@gmail.com>
2025-08-21 01:00:17 -0400

    Updated auth method

--- src/RequestHandler.py
+++ src/RequestHandler.py
@@ -12,24 +12,56 @@ class RequestHandler:
     def __init__(self) -> None:
 
         self._default_auth_config = {
-            "user_credentials": {"username": "", "password": ""},
-            "refresh_token": ""
+            "user_credentials": {
+                "username": "",
+                "password": "",
+                "client_id": "",
+                "client_secret": ""
+            },
+            "refresh_token": "",
+            "access_token": ""
         }
         self._username = ""
         self._password = ""
-        self._month_access_token = ""
-        self._jwt = ""
-        self._headers = {"Authorization": "Bearer " + self._jwt}
+        self._client_id = ""
+        self._client_secret = ""
+        self._access_token = ""
+        self._refresh_token = ""
+        self._headers = self.update_headers()
 
         self.mangadex_url_base = "https://api.mangadex.org"
+        self.mangadex_auth_url = "https://auth.mangadex.org"
 
 
     def config_exists(self, file="auth_config.yml"):
-        return os.path.isfile(file)
+        file_exists = os.path.isfile(file)
+        if not file_exists:
+            return False
+        else:
+            config_dict = {}
+            with open(file) as auth_configfile:
+                config_dict = yaml.safe_load(auth_configfile)
+                
+            # Check if config file expected keys have values. If not, return false
+            if len(config_dict) != 0 :
+                expected_creds = self._default_auth_config.get("user_credentials").keys()
+                actual_file_creds = config_dict.get("user_credentials")
+                if actual_file_creds is not None:
+                    all_creds_have_values = True
+                    for key in expected_creds:
+                        file_cred = actual_file_creds.get(key)
+                        if file_cred is None or file_cred == "":
+                            all_creds_have_values = False
+                            break
+                    return all_creds_have_values
+                else:
+                    return False
+            else:
+                return False
 
     def get_data_from_config(self, file="auth_config.yml"):
         if self.config_exists():
-            with open("auth_config.yml") as auth_configfile:
+            with open(file) as auth_configfile:
                 config_data = yaml.safe_load(auth_configfile)
                 return config_data
         else:
@@ -41,38 +73,54 @@ class RequestHandler:
 
     # Begin User Authentication
 
-    def set_user_credentials(self, username, password):
+    def set_user_credentials(self, username, password, client_id, client_secret):
         self._username = username
         self._password = password
+        self._client_id = client_id
+        self._client_secret = client_secret
         yaml_dict = self.get_data_from_config() if self.config_exists() else self._default_auth_config
         yaml_dict["user_credentials"]["username"] = username
         yaml_dict["user_credentials"]["password"] = password
+        yaml_dict["user_credentials"]["client_id"] = client_id
+        yaml_dict["user_credentials"]["client_secret"] = client_secret
         with open("auth_config.yml", "w") as configfile:
             yaml.safe_dump(yaml_dict, configfile)
 
 
-    def authenticate_user(self, username="", password=""):
+    def authenticate_user(self, username="", password="", client_id="", client_secret=""):
         """Authenticate the user"""
-        print(f"Authenticating user: {self._username}")
-        request_data = {
-            "username": self._username,
-            "password": self._password
-        }
-        self.auth_info = requests.post(self.mangadex_url_base + "/auth/login", json=request_data)
-        print("Authentication successful") if self.auth_info.status_code == 200 else print(f"Authentication failed, check your credentials: Error code {self.auth_info.status_code}")
-        
-        self._jwt = self.auth_info.json()["token"]["session"]
-        self._month_access_token = self.auth_info.json()["token"]["refresh"]
-        self._headers = {"Authorization": "Bearer " + self._jwt}
-        # Write refresh token to auth_configfile
         yaml_dict = self._default_auth_config
 
-        with open("auth_config.yml", "r") as auth_configfile:
-             yaml_dict = yaml.safe_load(auth_configfile)
-             yaml_dict["refresh_token"] = self._month_access_token
-        with open("auth_config.yml", "w") as auth_configfile:
-            yaml.safe_dump(yaml_dict, auth_configfile)
-        
+        print(f"Authenticating user: {self._username}")
+        request_data = {
+            "grant_type":"password",
+            "username": self._username if username == "" else username,
+            "password": self._password if password == "" else password,
+            "client_id": self._client_id if client_id == "" else client_id,
+            "client_secret": self._client_secret if client_secret == "" else client_secret
+        }
+        self.auth_info = requests.post(self.mangadex_auth_url + "/realms/mangadex/protocol/openid-connect/token", data=request_data)
+        if(self.auth_info.status_code == 200):
+            print("Authentication successful")
+             # Write refresh token to auth_configfile, keeping old values
+            self._access_token = self.auth_info.json()["access_token"]
+            self._refresh_token = self.auth_info.json()["refresh_token"]
+            self.update_headers()
+            self.update_tokens_in_cache(
+                access_token = self._access_token,
+                refresh_token = self._refresh_token
+            )
+        else:
+            print(f"Authentication failed, check your credentials: Error code {self.auth_info.status_code}")
+            error = self.auth_info.json()["error"]
+            error_desc = self.auth_info.json()["error_description"]
+            print(f"Error: {error}")
+            print(f"Error Description: {error_desc}")
+            # Reset the cached credentials
+            yaml_dict = self._default_auth_config
+            with open("auth_config.yml", "w") as auth_configfile:
+                yaml.safe_dump(yaml_dict, auth_configfile)
+            
         return self.auth_info.status_code, self.auth_info
 
     def get_logged_user_id(self):
@@ -83,14 +131,28 @@ class RequestHandler:
         else:
             print(f"Error fetching user id: Error code {response.status_code}")
         return response.status_code, response
-    
+
+
+    def update_tokens_in_cache(self, access_token="", refresh_token="", file="auth_config.yml"):
+        yaml_dict = self._default_auth_config
+        with open(file, "r") as auth_configfile:
+            yaml_dict = yaml.safe_load(auth_configfile)
+            yaml_dict["access_token"] = access_token if access_token!="" else self._access_token
+            yaml_dict["refresh_token"] = refresh_token if refresh_token!="" else self._refresh_token
+        with open(file, "w") as auth_configfile:
+            yaml.safe_dump(yaml_dict, auth_configfile)
+
+
     def refresh_session(self):
         """Refresh the temporary access token."""
-        data_json = {
-            "token": self._month_access_token
+        request_data = {
+            "grant_type": "refresh_token",
+            "refresh_token": self._refresh_token,
+            "client_id": self._client_id,
+            "client_secret": self._client_secret
         }
-        response = requests.post(self.mangadex_url_base + "/auth/refresh", json=data_json)
-        if response.status_code == 400:
+        response = requests.post(self.mangadex_auth_url + "/realms/mangadex/protocol/openid-conect/token", data=request_data)
+        if response.status_code == 400 or response.status_code == 404:
             self.authenticate_user()
         elif response.status_code == 429:
             # Too many requests
@@ -99,16 +161,20 @@ class RequestHandler:
             self.refresh_session()
         else:
             print(response.status_code)
-            self._jwt = response.json()["token"]["session"]
-            self._month_access_token = response.json()["token"]["refresh"]
-            self._headers = {"Authorization": "Bearer " + self._jwt}
+            self._access_token = response.json()["access_token"]
+            self.update_headers()
+            self.update_tokens_in_cache()
 
+    def update_headers(self, access_token=""):
+        new_token = access_token if access_token != "" else self._access_token
+        self._headers = {"Authorization": "Bearer " + new_token}
 
     # End User Authentication
 
 
     def get_username(self):
         return self._username
+
 
     def get_user_followed_manga_list(self):
         response = requests.get(self.mangadex_url_base + "/user/follows/manga", headers=self._headers)
@@ -119,7 +185,11 @@ class RequestHandler:
             self.follows_list = response.json()["data"]
             condensed_follows = {}
             for manga in self.follows_list:
-                condensed_follows.update({manga["attributes"]["title"]["en"]:{"id": manga["id"]}})
+                condensed_follows.update(
+                    {
+                        manga["attributes"]["title"]["en"]:{"id": manga["id"]}
+                    }
+                )
             return condensed_follows
         else:
             print(f"Error fetching follows list: Error code {response.status_code}")
