89d5a1d80fc1c9f69c51920a74c087b2c93f420b
Chris Stephenson <cgstephenson675@gmail.com>
2025-08-22 10:15:25 -0400

    Add export functionality to the ui

--- src/DataExporter.py
+++ src/DataExporter.py
@@ -1,29 +1,88 @@
+import json 
+import os
+from tkinter import filedialog
 from RequestHandler import RequestHandler
-import json
 
 class DataExporter:
 
     def __init__(self, request_handler=None):
         self.request_handler = request_handler if request_handler is not None else RequestHandler()
         self.credential_manager = self.request_handler.credential_manager
+        self.default_directory = "exports"
 
+    def export_follows_as_list_ui(self):
+        self.export_follows_ui(file_format="list")
+    def export_follows_as_json_ui(self):
+        self.export_follows_ui(file_format="json")
 
-    def export_follows_list(self, file="exports/user_follows.txt"):
-        print("Exporting follows list")
-        follows_list = self.request_handler.get_user_followed_manga_list().keys()
-        with open(file, "w") as follows_file:
-            follows_file.write('\n'.join(str(name) for name in follows_list))
+    def export_follows_ui(self, file_format="list"):
+        title = "Export Data"
+        if(file_format=="list"):
+            title += " as List"
+        elif(file_format=="json"):
+            title += " as JSON"
+        filepath = self.prompt_for_export_file(
+            title=title,
+            initial_file="user_follows",
+            file_extension=self.get_default_file_extension(file_format=file_format)
+        )
+        self.export_follows(file_format=file_format, file=filepath)
+
+    def prompt_for_export_file(self, title="Export Data", start_dir="", initial_file="", file_extension=".txt"):
+        if(start_dir==""):
+            start_dir = self.default_directory
+        if(initial_file!="" and "." not in initial_file):
+            initial_file+=file_extension
         
-    def export_follows_json(self, file="exports/user_follows.json"):
+        os.makedirs(start_dir, exist_ok=True)
+        return filedialog.askopenfilename(
+            title=title,
+            initialdir=start_dir,
+            initialfile=initial_file,
+            defaultextension=file_extension
+        )
+
+    def get_default_file_extension(self, file_format="list"):
+        if("json" in file_format):
+            return ".json"
+        elif(file_format=="list" or "txt" in file_format or file_format=="text"):
+            return ".txt"
+        else:
+            print("Unsupported format given: " + file_format + ". Defaulting to .txt")
+            return ".txt"
+
+    def export_follows(self, file_format="list", file=""):
+        """Export follows list
+        
+        params:
+            format -> Either 'list' or 'json'
+            file -> the file path to write the file to. Will default to using the "exports" folder
+        """
         print("Exporting follows list")
         follows_list = self.request_handler.get_user_followed_manga_list()
+        
+        # Update default file extension
+        if(file==""):
+            file = self.default_directory + "/" + "user_follows" + self.get_default_file_extension(file_format=file_format)
+
+        # Write to file
         with open(file, "w") as follows_file:
-            follows_file.write(json.dumps(follows_list, indent=2))
+            if(file_format=="list"):
+                follows_file.write('\n'.join(str(name) for name in follows_list.keys()))
+            elif(file_format=="json"):
+                follows_file.write(json.dumps(follows_list, indent=2))
+        if(os.path.isfile(file)):
+            print("Export successful")
+        else:
+            print("Export failed to create file")
+                
 
 def main():
     dataExporter = DataExporter()
-    dataExporter.export_follows_json()
-    dataExporter.export_follows_list()
+    dataExporter.export_follows()
+    dataExporter.export_follows(file_format="json")
+    dataExporter.export_follows_ui()
+    dataExporter.export_follows_ui("json")
     pass
 
 if __name__ == "__main__":
--- src/MainWindow.py
+++ src/MainWindow.py
@@ -3,6 +3,7 @@ from SearchFrame import SearchFrame
 from UserInfoFrame import UserInfoFrame
 from RequestHandler import RequestHandler
 from CredentialManager import CredentialManager
+from DataExporter import DataExporter
 
 class MainWindow(tk.Tk):
     
@@ -14,20 +15,34 @@ class MainWindow(tk.Tk):
         self.minsize(minsize[0], minsize[1])
         
         self.request_handler = RequestHandler(credential_manager=CredentialManager())
+        self.data_exporter = DataExporter(self.request_handler)
+
         self.user_info_frame = UserInfoFrame(self.request_handler, self)
         self.search_frame = SearchFrame(self.request_handler, self)
         self.create_menu_bar()
 
     def create_menu_bar(self):
         self.menu_bar = tk.Menu(self)
+        
         user_menu = tk.Menu(self.menu_bar, tearoff=0)
         user_menu.add_command(label="Refresh follows list", command=self.user_info_frame.create_follows_list_widget)
         user_menu.add_command(label="Logout")
         self.menu_bar.add_cascade(label="Profile", menu=user_menu)
         self.menu_bar.add_command(label="Random Manga", command=self.open_random_manga)
-        self.menu_bar.add_cascade(label="Export", )
+        
+        self.create_export_menu_option()
+
         self.menu_bar.add_command(label="Exit", command=self.destroy)
         self.config(menu=self.menu_bar)
+
+    def create_export_menu_option(self):
+        export_menu = tk.Menu(self.menu_bar, tearoff=0)
+        follows_export_menu = tk.Menu(export_menu, tearoff=0)
+        follows_export_menu.add_command(label="List", command=self.data_exporter.export_follows_as_list_ui)
+        follows_export_menu.add_command(label="JSON", command=self.data_exporter.export_follows_as_json_ui)
+        export_menu.add_cascade(label="Follows List", menu=follows_export_menu)
+        self.menu_bar.add_cascade(label="Export", menu=export_menu)
+
 
     def open_random_manga(self):
         pass
